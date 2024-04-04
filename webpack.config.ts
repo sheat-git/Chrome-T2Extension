5512b6d0747eaf0ca5d233567b60b4ba65b4faee
sheat <dev.sheat@gmail.com>
2024-04-04 16:33:54 +0900

    Format

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,41 +8,43 @@ const config: Configuration = {
     options: './src/options.tsx',
     authenticate: './src/authenticate.tsx',
     redirect: './src/redirect.ts',
-    background: './src/background.ts'
+    background: './src/background.ts',
   },
   output: {
     path: `${__dirname}/dist`,
     publicPath: './',
-    filename: 'js/[name].js'
+    filename: 'js/[name].js',
   },
   module: {
     rules: [
       {
         test: /.tsx?$/,
-        use: 'ts-loader'
-      }
-    ]
+        use: 'ts-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js', '.tsx']
+    extensions: ['.ts', '.js', '.tsx'],
   },
   plugins: [
     new CopyPlugin({
-      patterns: [{
-        from: '.',
-        to: '.',
-        context: 'public'
-      }]
+      patterns: [
+        {
+          from: '.',
+          to: '.',
+          context: 'public',
+        },
+      ],
     }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       chunks: ['options'],
       title: 'T2Extension Settings',
-      filename: 'options.html'
-    })
+      filename: 'options.html',
+    }),
   ],
   devtool: process.env.NODE_ENV === 'production' ? false : 'cheap-source-map',
-  cache: false
+  cache: false,
 }
 
 export default config
